694cfa71070ec3180a46b73efd87a7f45ab8ed4b
Luis González <ljgonzalez@uc.cl>
2025-12-28 11:11:59 -0300

    🔧 chore(platformio.ini): update upload port from /dev/ttyACM0 to /dev/ttyACM1
    ✨ feat(main.cpp): initialize knob button functionality in app_main
    
    ✨ feat(knob_button.cpp): implement knob button handling with debounce logic
    
    ✨ feat(knob_button.hpp): add header for knob button initialization and event logging

--- example-03-pushbutton/src/main.cpp
+++ example-03-pushbutton/src/main.cpp
@@ -2,9 +2,15 @@
 #include "freertos/task.h"
 
 #include "check_libs/check_libs.hpp"
+#include "config_pins.h"
+#include "knob_button/knob_button.hpp"
 
 extern "C" [[noreturn]] void app_main(void) {
+  // Mantienes tu checker
   viewe_show_imported_libs();
+
+  // Arrancas el botón (pulsación de la perilla)
+  knob_button_start(PIN_SW);
 
   while (true) {
     vTaskDelay(pdMS_TO_TICKS(500));
